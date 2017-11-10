b8b9dd9daef75062e65f605a693caa59adec47b2
MMz <>
2017-11-10 21:23:29 +0100

    Add constantly function

--- src/stdlib/core.clj
+++ src/stdlib/core.clj
@@ -1,11 +1,48 @@
 (ns stdlib.core)
 
+
+;;;;;;;;;;;;;;;;
+;; CONSTANTLY ;;
+;;;;;;;;;;;;;;;;
+
+
+;; Returns a function that always returns the given argument
+((constantly 42))
+
+;; My implementation
+(defn constantly* [x] (fn [& _] x))
+
+(def always42 (constantly 42))
+(always42)
+(always42 :a)
+(always42 nil [] false "blah")
+
+;; Use it when a lib requires a function, but you want to provide a value
+;; So instead of:
+(update [0 1 2 3] 3 (fn [x] (+ x 72)))
+;; You provide a function that returns the value:
+(update [0 1 2 3] 3 (constantly 75))
+
+;; Use it when you want the same value x times in a sequence
+(map (constantly "MM") (range 10))
+
+;; Use it as a stub
+(defn some-hard-to-setup-library-fn [& _] "return something complex")
+(some-hard-to-setup-library-fn :x :& :y)
+(with-redefs [some-hard-to-setup-library-fn (constantly "simple")]
+  (= "simple" (some-hard-to-setup-library-fn "with" "complex" "params")))
+
+
 ;;;;;;;;;;;;;;
 ;; IDENTITY ;;
 ;;;;;;;;;;;;;;
 
+
 ;; Returns its argument
 (identity 1)
+
+;; My implementation
+(defn identity* [x] x)
 
 ;; Used to filter 'logical false' elements (`nil` or `false`)
 (filter identity [1 2 nil 4 false true :a])
@@ -28,3 +65,8 @@
 
 ;; Group by identity:
 (group-by identity "abraacadaabra")
+
+
+;;;;;;;;;;;;;;;;;
+;; WITH-REDEFS ;;
+;;;;;;;;;;;;;;;;;
