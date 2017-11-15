e9557dfee56117850754c9c42e343f91c85a2a53
MMz <mmz.source@gmail.com>
2017-11-15 23:07:07 +0100

    Recursion - Add own map implementation

--- src/stdlib/recursion.clj
+++ src/stdlib/recursion.clj
@@ -1,6 +1,7 @@
 (ns stdlib.recursion)
 
-;; Classic recursion:
+;; Classic recursion
+;; -----------------
 ;; 1. Base Case
 ;; 2. Advance through the datastructure (divide the problem)
 ;; 3. Initialisation
@@ -23,3 +24,18 @@
 (count* [1 2 3])
 (count* {:a 1 :b 2})
 (count* (range 7))
+
+
+;;;;;;;;;
+;; MAP ;;
+;;;;;;;;;
+
+(map inc [2 3 4])
+
+;; My implementation
+(defn map* [f coll]
+  (if (empty? coll)
+    ()
+    (cons (f (first coll)) (map* f (rest coll)))))
+
+(map* inc [1 2 3 4 5 6])
