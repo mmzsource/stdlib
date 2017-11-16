f30b1ac424c4bcf4efebc352b96c3afe57bcc8e3
MMz <mmz.source@gmail.com>
2017-11-16 10:07:45 +0100

    Add 3 filter implementations to expore tail call recursion

--- src/stdlib/recursion.clj
+++ src/stdlib/recursion.clj
@@ -5,6 +5,12 @@
 ;; 1. Base Case
 ;; 2. Advance through the datastructure (divide the problem)
 ;; 3. Initialisation
+;;
+;; Two types of recursion
+;; ----------------------
+;; 1. Tail recursion (recur) - Doesn't have to remember anything
+;; 2. Non tail recursion - Has to remember where it's at. Stackoverflow risk.
+;;
 
 ;;;;;;;;;;;
 ;; COUNT ;;
@@ -39,3 +45,40 @@
     (cons (f (first coll)) (map* f (rest coll)))))
 
 (map* inc [1 2 3 4 5 6])
+
+;;;;;;;;;;;;
+;; FILTER ;;
+;;;;;;;;;;;;
+
+(filter even? (range 10))
+
+;; My implementation
+(defn filter* [pred coll]
+  (if (empty? coll)
+    ()
+    (if (pred (first coll))
+      (cons (first coll) (filter* pred (rest coll)))
+      (filter* pred (rest coll)))))
+
+(filter* even? (range 10))
+
+;; Implementation with (partial) tail recursion:
+(defn filter** [pred coll]
+  (if (empty? coll)
+    ()
+    (if (pred (first coll))
+      (cons (first coll) (filter* pred (rest coll)))
+      (recur pred (rest coll)))))
+
+(filter** even? (range 10))
+
+;; Implementation with complete tail recursion:
+(defn filter*** [pred coll]
+  ((fn [pred coll acc]
+      (if (empty? coll)
+        acc
+        (if (pred (first coll))
+          (recur pred (rest coll) (conj acc (first coll)))
+          (recur pred (rest coll) acc)))) pred coll []))
+
+(filter*** even? (range 10))
